1a36f462e491d963770d2f4cfc393395c14cf6fd
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-16 11:37:18 +0600

    Check if two strings are anagrams

---
+++ Anagram.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        boolean isAnagram = areAnagrams(str1, str2);
+        System.out.println("Are anagrams? " + isAnagram);
+    }
+
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+}
