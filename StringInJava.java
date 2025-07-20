382395eced5ceacaa2d2322ce8d882f31d89e3aa
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-20 09:34:04 +0600

    add two string using concate() function

---
+++ StringInJava.java
@@ -0,0 +1,8 @@
+public class StringInJava {
+    public static void main(String args[]){
+        String s = "Hello, World";
+        String stringUpdate=s.concat(" I am here");
+
+        System.out.println(stringUpdate);
+    }
+}
